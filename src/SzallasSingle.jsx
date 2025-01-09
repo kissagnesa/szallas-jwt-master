0fd385e3d6814868b2195770e525809e8dc49e8e
kissagnesa <sindzse88@gmail.com>
2025-01-09 16:52:15 +0100

    c

--- src/SzallasSingle.jsx
+++ src/SzallasSingle.jsx
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import {Link} from 'react-router-dom';
+import {Link, useParams} from 'react-router-dom';
 import axios from 'axios';
 
 export const SzallasList = () => {
     const [data, setData] = useState([]);
     const [error, setError] = useState('');
+    const params=useParams;
+    const id=params.id;
 
     useEffect(() => {
         const fetchData = async () =>{
@@ -13,7 +15,7 @@ export const SzallasList = () => {
                 if(!token) {
                     throw new Error('Nem található JWT token!');
                 }
-                const valasz = await axios.get('https://szallasjwt.sulla.hu/data', {
+                const valasz = await axios.get('https://szallasjwt.sulla.hu/data' , {
                     headers: {
                         Authorization: `Bearer ${token}`
                     }
@@ -29,17 +31,17 @@ export const SzallasList = () => {
     }, []);
     return (
         <div>
-        <h2>Szállások listája</h2>
+        <h2>Egy szállás adatai</h2>
         {error && <p style={{ color: 'red'}}> {error} </p>}
         {data.length>0 ? (
           <ul> 
-          { data.map((item) => (
-          <li key={item.id}>{item.name} - {item.hostname} - {item.location} - {item.price} - {item.minimum_nights}
-         <Link to={"/data/" + item.id}><i className="bi bi-text-paragraph fs-6 btn btn-primary"></i></Link>&nbsp;&nbsp;&nbsp;
-         <Link to={"/data-mod/" + item.id}><i className="bi bi-pencil-square fs-6 btn btn-warning"></i></Link>&nbsp;&nbsp;&nbsp;
-         <Link to={"/data-del/" + item.id}><i className="bi bi-trash3 fs-6 btn btn-danger"></i></Link><br /><br />
+         
+          <li key={data.id}>{data.name} - {data.hostname} - {data.location} - {data.price} - {data.minimum_nights}
+         <Link to={"/data/"}><i className="bi bi-backspace fs-6 btn btn-primary"></i></Link>&nbsp;&nbsp;&nbsp;
+         <Link to={"/data-mod/" + data.id}><i className="bi bi-pencil-square fs-6 btn btn-warning"></i></Link>&nbsp;&nbsp;&nbsp;
+         <Link to={"/data-del/" + data.id}><i className="bi bi-trash3 fs-6 btn btn-danger"></i></Link><br /><br />
           </li>
-        ))}
+        
         </ul> ) : ( <p>Nem találhatók az adatok!</p>)
     } 
     </div>
